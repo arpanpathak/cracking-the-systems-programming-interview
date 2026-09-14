8563b9252126097980c81ce3d40e2fad4a4f6213
arpanpathak <arpan.pathak47@gmail.com>
2026-09-13 18:49:28 -0700

    pages: publish the mdBook and the PDF

--- ll.rs
+++
@@ -1,9 +0,0 @@
-using std::collctions::HashMap;
-
-fn main() {
-    let mut map = HashMap<i32>::new();
-
-    map.insert(2, "Name");
-
-    println!("Map value got {}", map.get(2).unwrap_or("Abc"));
-}
