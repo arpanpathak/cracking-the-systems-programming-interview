4e6f163aedb8578b197f1e006d26dee9b647b6e7
arpanpathak <arpan.pathak47@gmail.com>
2026-09-14 17:03:37 -0700

    pages: publish chapter 64

--- book/searcher-09f2665d.js
+++ book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-22ab09d0.js',
+                path_to_root + 'searchindex-f56fc872.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
